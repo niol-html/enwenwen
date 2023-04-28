97878c64082023a7575d9de3d7dc5606850a15f5
niol-html <115099363+niol-html@users.noreply.github.com>
2023-04-27 21:55:26 -0400

    comit

--- script.js
+++
@@ -1,9 +0,0 @@
-function removeLoading() {
-    setTimeout(function() {
-      $("#landingpage").removeClass("is-loading");
-    }, 100);
-  }
-  
-  $(document).ready(function() {
-    removeLoading();
-  });
